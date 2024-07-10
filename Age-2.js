61afe95d491d1f8c34ab7af921b307b9f7d18926
anik <anirudhamajumder68@gmail.com>
2024-07-10 09:51:01 +0600

    using of swtich

---
+++ Age-2.js
@@ -0,0 +1,23 @@
+
+const readlineSync = require("readline-Sync");
+let age=readlineSync.question("what is your age?" );
+age=parseInt(age);
+switch(age){
+    case 12:
+        console.log("Your age is 12");
+        break;
+    case 13:
+        console.log("Your age is 13");
+        break;
+    case 14:
+        console.log("Your age is 14");
+        break;
+    case 15:
+        console.log("Your age is 15");
+        break;
+    case 16:
+        console.log("Your age is 16");
+        break;
+    default:
+        console.log("Your age is not so special");
+}
