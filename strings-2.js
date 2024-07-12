a417d6e0cdd5a395885a117115d040d4dd541133
anik <anirudhamajumder68@gmail.com>
2024-07-12 12:14:21 +0600

    Added JavaScript code for string operations and console logs.

---
+++ strings-2.js
@@ -0,0 +1,10 @@
+let boy1="     Shanto      ";
+let boy2="Farhan"
+let friut="man\"go";
+console.log(friut)
+console.log(boy1.toUpperCase());
+console.log(boy1.toLowerCase());
+console.log(boy2.slice(2,4));
+console.log(boy2.slice(2));
+console.log(boy1.replace("Sha","Kla"));
+console.log(boy1.concat(" is a friend of ",boy2," okk"));
